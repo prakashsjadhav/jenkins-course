0a76ff04c4d7f8f9d969f661f128fbfb732cc935
Prakash Jadhav <53575105+prakashsjadhav@users.noreply.github.com>
2021-07-22 13:09:58 +0530

    Update nodejspipelinedocker.groovy

--- job-dsl/nodejspipelinedocker.groovy
+++ job-dsl/nodejspipelinedocker.groovy
@@ -1,32 +1,31 @@
-- script: >
 pipelineJob('NodeJS Pipeline Job DSL Docker example') {
   definition {
     cpscm { 
-      scriptPath 'misc/Jenkinsfile'
       scm {
         git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
+            scriptPath('misc/Jenkinsfile')
         }
       }
-      triggers {
-        scm('H/5 * * * *')
-      }
-      wrappers {
+    }
+  }
+  triggers {
+    scm('H/5 * * * *')
+  }
+  wrappers {
         nodejs('nodejs') // this is the name of the NodeJS installation in 
                          // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-      }
-      steps {
-        dockerBuildAndPublish {
-            repositoryName('prakashsjadhav2019/docker-nodejs-demo')
-            tag('${GIT_REVISION,length=9}')
-            registryCredentials('dockerhub')
-            forcePull(false)
-            forceTag(false)
-            createFingerprints(false)
-            skipDecorate()
-        }
-      }
+  }
+  steps {
+    dockerBuildAndPublish {
+      repositoryName('prakashsjadhav2019/docker-nodejs-demo')
+      tag('${GIT_REVISION,length=9}')
+      registryCredentials('dockerhub')
+      forcePull(false)
+      forceTag(false)
+      createFingerprints(false)
+      skipDecorate()
     }
   }
 }
