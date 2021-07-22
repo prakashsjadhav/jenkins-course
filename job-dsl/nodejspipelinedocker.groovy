3239d0658ebf5ba8adf31eaa8a2282216e1d2538
Prakash Jadhav <53575105+prakashsjadhav@users.noreply.github.com>
2021-07-22 14:01:24 +0530

    Update nodejspipelinedocker.groovy

--- job-dsl/nodejspipelinedocker.groovy
+++ job-dsl/nodejspipelinedocker.groovy
@@ -1,27 +1,15 @@
-job('NodeJS Pipeline Job DSL Docker example') {
+pipelineJob('job-dsl-plugin') {
+  definition {
+    cpsScm {
       scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
-            node / gitConfigName('DSL User')
-            node / gitConfigEmail('jenkins-dsl@newtech.academy')
-            scriptPath('misc/Jenkinsfile')
+        git {
+          remote {
+            url('https://github.com/jenkinsci/job-dsl-plugin.git')
+          }
+          branch('*/master')
         }
       }
-  triggers {
-    scm('H/5 * * * *')
-  }
-  wrappers {
-        nodejs('nodejs') // this is the name of the NodeJS installation in 
-                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
-  }
-  steps {
-    dockerBuildAndPublish {
-      repositoryName('prakashsjadhav2019/docker-nodejs-demo')
-      tag('${GIT_REVISION,length=9}')
-      registryCredentials('dockerhub')
-      forcePull(false)
-      forceTag(false)
-      createFingerprints(false)
-      skipDecorate()
+      lightweight()
     }
   }
 }
