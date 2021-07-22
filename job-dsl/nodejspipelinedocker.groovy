8fed517c2a684a44e6de6baf1c8d44b43c52815f
Prakash Jadhav <53575105+prakashsjadhav@users.noreply.github.com>
2021-07-22 14:21:15 +0530

    Update nodejspipelinedocker.groovy

--- job-dsl/nodejspipelinedocker.groovy
+++ job-dsl/nodejspipelinedocker.groovy
@@ -11,4 +11,22 @@ pipelineJob('NodeJS Pipeline Job DSL Docker example') {
       }
 	}
   }
+  triggers {
+    scm('H/5 * * * *')
+  }
+  wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+  }
+  steps {
+    dockerBuildAndPublish {
+      repositoryName('prakashsjadhav2019/docker-nodejs-demo')
+      tag('${GIT_REVISION,length=9}')
+      registryCredentials('dockerhub')
+      forcePull(false)
+      forceTag(false)
+      createFingerprints(false)
+      skipDecorate()
+    }
+  }
 }
